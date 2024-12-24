e9b8da6d7b7b1de35534da42ae2ad3c421096311
David Dubins <d.dubins@utoronto.ca>
2024-12-23 22:41:23 -0500

    Update 1servo.ino

--- examples/1servo/1servo.ino
+++ examples/1servo/1servo.ino
@@ -3,11 +3,13 @@
 // Author: D.Dubins
 // Date: 23-Dec-24
 // Controlling 1 servo on any digital pin in BANK A or B from PA0 to PB2 on ATtiny84 (clock frequency=8MHz)
-// The following pin numbers are used:
-// servo #0..7: PA0..PA7 (8 servos)
-// servo #8..10: PB0..PB2 (3 servos)
+// The library maps specific servo numbers to the following pins:
+// servo 0: PA0   servo 3: PA3   servo 6: PA6   servo 9: PB1
+// servo 1: PA1   servo 4: PA4   servo 7: PA7   servo 10: PB2
+// servo 2: PA2   servo 5: PA5   servo 8: PB0
 
 #include "tinyServo84.h"
+byte motor1 = 2; // motor 1 is attached to PA2
 
 // Change these values to suit your application:
 #define SVOMAXANGLE 179   // maximum angle for servo.
@@ -19,8 +21,8 @@ tinyServo84 myServos;     // declare object called myServos of class tinyServo84
 
 void setup() {
   myServos.setCTC();
-  myServos.attachServo(0); // attach servo0 (PA0)
-  myServos.homeServos();   // home any attached servos
+  myServos.attachServo(motor1); // attach motor1
+  myServos.homeServos(); // home servo0
 }
 
 void loop() {
@@ -31,43 +33,43 @@ void loop() {
   // (Default=0, which means any change in signal will wake up servos).
   // The only reason you might need a number tolerance is if you would like to disable the timer, and you
   // are reading noisy potentiometer readings (suggested: ~10 per servo enabled).
-  //myServos.servo_timeout_check(0);  // if servos are inactive, stop Timer1 (less trouble for other routines)
+  myServos.servo_timeout_check(0);  // if servos are inactive, stop Timer1 (less trouble for other routines)
   
   //Uncomment for rocking servo 0 at full speed.
-  /*myServos.setServo(0, 0);
+  /*myServos.setServo(motor1, 0);
   delay(1000);
-  myServos.setServo(0, SVOMAXANGLE);
+  myServos.setServo(motor1, SVOMAXANGLE);
   delay(1000);*/
 
   // Uncomment to rock servo 0 using moveTo(), with a 10ms delay between steps:
-  moveTo(0, 10);
-  moveTo(SVOMAXANGLE, 10);
+  //moveTo(motor1, 0, 10);
+  //moveTo(motor1, SVOMAXANGLE, 10);
 
   // Uncomment to move servo 0 slowly (0.5 second steps)
   /*for (int i = 0; i < SVOMAXANGLE; i++) {
-    myServos.setServo(0, i);
+    myServos.setServo(motor1, i);
     delay(500); // 0.5s delay should let you see each angle
   }
   for (int i = SVOMAXANGLE; i >=0; i--) {
-    myServos.setServo(0, i);
+    myServos.setServo(motor1, i);
     delay(500);
   }*/
 
   // Uncomment for potentiometer control:
   //int location = map(analogRead(A7), 1023, 0, 0, SVOMAXANGLE); // take pot reading from pin A7 & remap to angle.
-  //myServos.setServo(0, location);  // write new location to servo 0
+  //myServos.setServo(motor1, location);  // write new location to servo 0
   //delay(50);              // wait a bit to reduce jittering
 
   // Uncomment for potentiometer control of servo0 with smoother movement:
-  //int location = map(analogRead(A7), 1023, 0, 0, SVOMAXANGLE); // take pot reading from pin A7 & remap to angle.
-  //moveTo(location, 5);  // move to new location, delay=4 ms between steps
+  int location = map(analogRead(A7), 1023, 0, 0, SVOMAXANGLE); // take pot reading from pin A7 & remap to angle.
+  moveTo(motor1, location, 5);  // move to new location, delay=4 ms between steps
 }
 
-void moveTo(int s0, int wait) { // routine to move servo slower (more smoothly)
+void moveTo(byte servo_num, int s0, int wait) { // routine to move servo slower (more smoothly)
   int loc = s0; // store s0 to loc
   static int pos; // keep track of current position
   if (wait == 0) { // option to move as fast as possible
-    myServos.setServo(0, loc); // send new location to servo
+    myServos.setServo(servo_num, loc); // send new location to servo
   } else {
     int dev = 0; // to keep track of deviation from s0
     do {
@@ -75,7 +77,7 @@ void moveTo(int s0, int wait) { // routine to move servo slower (more smoothly)
       if (loc > pos) pos++;
       if (loc < pos) pos--;
       dev += abs(pos - loc);
-      myServos.setServo(0, pos);
+      myServos.setServo(servo_num, pos);
       delay(wait); // wait a bit (slows down movement)
     } while (dev > 0);
   }
